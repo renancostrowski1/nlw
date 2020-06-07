9798a2743eba3d1fb4722c7e3eee11af8a1f8a9d
Renan Ostrowski <renan_ostrowski@hotmail.com>
2020-06-07 15:19:31 -0300

    Projeto em fase final

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from 'express';
+import knex from '../database/connection';
+
+class ItemsController{
+    async index(request: Request, response: Response) {
+        const items = await knex('items').select('*');
+    
+        const serealizedItems = items.map(item => {
+            return{
+                id: item.id,
+                title: item.title,
+                image_url : `http://192.168.0.109:3333/uploads/${item.image}`,
+            }
+        });
+    
+        return response.json(serealizedItems);
+    }
+}
+
+export default ItemsController;
